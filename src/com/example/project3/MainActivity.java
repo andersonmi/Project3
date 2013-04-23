03629df69b8568c285e3b5a71cb39964ffcd9e1e
andersonmi <anderson.mi91@gmail.com>
2013-04-23 14:11:05 -0400

    added base gps code

--- src/com/example/project3/MainActivity.java
+++ src/com/example/project3/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.project3;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    private LocationManager locationManager = null;
+    private ActiveListener activeListener = new ActiveListener();
+    
+    private double latitude;
+    private double longitude;
 	
 	private static final int GOT_COLOR = 1;
 	
@@ -17,6 +29,7 @@ public class MainActivity extends Activity {
 	private static final String STROKE = "stroke";
 
 	private DrawingView drawingView;
+	private TextView currentLocation;
 	
 	
 	@Override
@@ -25,17 +38,22 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN); 
 		
+		locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		
 				
 		/*
 		 * Get Views
 		 */
 		drawingView = (DrawingView)findViewById(R.id.drawingView1);
+		currentLocation = (TextView)findViewById(R.id.curLocation);
         
         /*
          * Make drawing view editable
          */
         drawingView.setEditable(true);
 		
+        setUI();
+        
 		/*
          * Restore any state
          */
@@ -100,6 +118,104 @@ public class MainActivity extends Activity {
 		
 		drawingView.putToBundle(DRAWING, outState);
 	}
+	
+	
+	/*
+	 * GPS STUFF
+	 */
+	
+	private void setUI() {
+		Log.i("ONLOCATION", "New location registered");
+		currentLocation.setText("(" + latitude + ", " + longitude + ")");
+	}
+	
+	   
+   private void onLocation(Location location) {
+	   if(location == null) {
+           return;
+       }
+       
+       latitude = location.getLatitude();
+       longitude = location.getLongitude();
+       
+       setUI();
+       
+   }
+	   
+
+   
+   private void unregisterListeners() {
+       locationManager.removeUpdates(activeListener);
+   }
+   
+   private void registerListeners() {
+       unregisterListeners();
+       
+       // Create a Criteria object
+       Criteria criteria = new Criteria();
+       criteria.setAccuracy(Criteria.ACCURACY_FINE);
+       criteria.setPowerRequirement(Criteria.POWER_HIGH);
+       criteria.setAltitudeRequired(true);
+       criteria.setBearingRequired(false);
+       criteria.setSpeedRequired(false);
+       criteria.setCostAllowed(false);
+       
+       String bestAvailable = locationManager.getBestProvider(criteria, true);
+       
+       
+       if(bestAvailable != null) {
+           locationManager.requestLocationUpdates(bestAvailable, 500, 1, activeListener);
+           //TextView viewProvider = (TextView)findViewById(R.id.textProvider);
+         //  viewProvider.setText(bestAvailable);
+           Location location = locationManager.getLastKnownLocation(bestAvailable);
+           onLocation(location);
+       }
+   }
+	
+
+  /**
+    * The activity is being paused
+    */
+   @Override
+   protected void onPause() {
+       unregisterListeners();
+       super.onPause();
+   }
+
+   /**
+    * The activity is being resumed
+    */
+   @Override
+   protected void onResume() {
+       super.onResume();
+       registerListeners();
+   }
+   
+   
+   private class ActiveListener implements LocationListener {
+
+       @Override
+       public void onLocationChanged(Location location) {
+           onLocation(location);
+       }
+
+       @Override
+       public void onProviderDisabled(String provider) {
+           registerListeners();
+       }
+
+       @Override
+       public void onProviderEnabled(String provider) {
+           
+       }
+
+       @Override
+       public void onStatusChanged(String provider, int status, Bundle extras) {
+
+       }
+
+       
+   };
 	
 
 }
